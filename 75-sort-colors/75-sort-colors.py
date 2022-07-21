222015379522e44e4f1a5bfc1a38fb28c5d31006
VarunRaj Badri <54031828+VarunYadav99@users.noreply.github.com>
2022-07-21 18:34:48 +0530

    Time: 57 ms (37.77%), Space: 13.8 MB (97.26%) - LeetHub

--- 75-sort-colors/75-sort-colors.py
+++ 75-sort-colors/75-sort-colors.py
@@ -1,9 +1,7 @@
-class Solution:
+"""class Solution:
     
     def sortColors(self, nums: List[int]) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
+        
         
         # constant for colors
         RED, WHITE, BLUE = 0, 1, 2
@@ -34,4 +32,21 @@ class Solution:
             
             
             # i move forward
-            i += 1                
+            i += 1  """   
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        l,m,h = 0,0,len(nums)-1
+        while(m<=h):
+            if nums[m] == 0:
+                nums[l],nums[m] = nums[m],nums[l]
+                l +=1
+                m +=1
+            elif nums[m] == 1:
+                m +=1
+            elif nums[m] == 2:
+                nums[h], nums[m] = nums[m],nums[h]
+                h -=1
+        return nums
