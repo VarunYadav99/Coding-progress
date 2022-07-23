64c49fc19ce6665729bd3a73130a29bcfea11dae
VarunRaj Badri <54031828+VarunYadav99@users.noreply.github.com>
2022-07-23 14:35:18 +0530

    Time: 1135 ms (44.35%), Space: 26.4 MB (98.93%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.py
+++ 11-container-with-most-water/11-container-with-most-water.py
@@ -1,4 +1,4 @@
-class Solution:
+"""class Solution:
     def maxArea(self, height: List[int]) -> int:
         
         # length of input array
@@ -33,3 +33,48 @@ class Solution:
                 
         return area
                 
+"""
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        l, r = 0, len(height) - 1
+        res = 0
+
+        while l < r:
+            res = max(res, min(height[l], height[r]) * (r - l))
+            if height[l] < height[r]:
+                l += 1
+            elif height[r] <= height[l]:
+                r -= 1
+        return res
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
