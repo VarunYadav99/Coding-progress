ed9f8a3f62cbb4eaec9e646516a959befd16dadf
VarunRaj Badri <54031828+VarunYadav99@users.noreply.github.com>
2022-07-26 11:44:32 +0530

    Time: 39 ms (74.43%), Space: 13.9 MB (9.79%) - LeetHub

--- 367-valid-perfect-square/367-valid-perfect-square.py
+++ 367-valid-perfect-square/367-valid-perfect-square.py
@@ -1,11 +1,26 @@
 """#O(sqrt(n))"""
-class Solution:
+"""class Solution:
     def isPerfectSquare(self, num: int) -> bool:
-        for i in  range(1,num + 1):
-            if i * i == num:
+        for i in range(1,num+1)
+             if i * i = num 
+                return true
+             if i * i> num
+             return False
+             """
+class Solution:
+    def isPerfectSquare(self ,num: int) ->bool:
+        low,high = 1,num
+        while low <= high:
+            mid = (low + high) // 2
+            if mid * mid > num:
+                 high = mid - 1
+            elif mid * mid < num:
+                 low = mid + 1
+            else:
                 return True
-            if i * i > num:
-                return False 
-            
+        return False
+        
+    
+
         
         
